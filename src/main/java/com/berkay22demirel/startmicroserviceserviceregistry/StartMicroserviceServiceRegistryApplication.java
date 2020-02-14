f0a557199d4935faa342f3a31abe2b93541a069d
berkay22demirel <Rj6x355pl>
2020-02-14 09:22:58 +0300

    Initial commit

---
+++ src/main/java/com/berkay22demirel/startmicroserviceserviceregistry/StartMicroserviceServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.berkay22demirel.startmicroserviceserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartMicroserviceServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartMicroserviceServiceRegistryApplication.class, args);
+	}
+
+}
